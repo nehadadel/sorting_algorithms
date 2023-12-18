ad49753bb90f56a31e6eff3803b5de13b5821c6d
nehadadel <eng.nehadadel@gmail.com>
2023-12-18 12:29:24 -0800

    s

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ *insertion_sort_list - sorts a doubly linked list of integers in
+ *ascending order using the Insertion sort algorithm
+ *
+ *@list: list to sorted
+*/
+void insertion_sort_list(listint_t **list)
+{
+
+}
--- sort.h
+++ sort.h
@@ -21,5 +21,7 @@ struct listint_s *next;
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif
